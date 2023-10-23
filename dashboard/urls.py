8de6f19a8b8ace760be28bdb0c5e9ea14d845056
kebaHailu <kibromhailu.5513@gmail.com>
2023-10-23 11:57:43 +0300

    Finishing the hosting part

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,7 +4,7 @@ from . import views
 # the following code will create the url for the dashboard app
 urlpatterns = [
     
-    path('products/',views.products, name='dashboard-products'),
-    path('products/delete/<int:pk>/',views.product_delete, name='dashboard-product-delete'),
-    path('products/update/<int:pk>/',views.product_update, name='dashboard-product-update'),
+    path('',views.products, name='dashboard-products'),
+    path('delete/<int:pk>/',views.product_delete, name='dashboard-product-delete'),
+    path('update/<int:pk>/',views.product_update, name='dashboard-product-update'),
 ]
